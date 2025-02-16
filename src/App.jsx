e72be00bf2e3c109b8172f16f1a49c296de20c16
briannjara <briannjaramba138@gmail.com>
2025-02-16 17:55:44 +0300

    second-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
 
 
@@ -17,7 +19,8 @@ const App = () => {
         <Collaboration/>
         <Services/>
         <Pricing/>
-        
+        <Roadmap/>
+        <Footer/>        
       </div>
 
       <ButtonGradient />
